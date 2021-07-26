f8df090e16dab19d9c9f13532f48aaece786c636
reid23 <reid.dye@gmail.com>
2021-07-26 13:32:47 -0700

    started working on path following

--- path-following.py
+++ path-following.py
@@ -1,2 +1,13 @@
 # converts path and odometry data, using a PID loop and a path following algorithm (currently pure pursuit)
 # to generate send_rc_control values that can be sent by main.py
+
+import scipy
+import numpy as np
+
+
+class path():
+    points = []
+
+
+def followPath(path):
+    pass
