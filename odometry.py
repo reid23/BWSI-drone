78fb3307f9958b0f301759826cce79a0b2255235
reid23 <reid.dye@gmail.com>
2021-07-26 20:30:06 -0700

    baseline odometry class made, need to fill out other classes more

--- odometry.py
+++ odometry.py
@@ -6,11 +6,21 @@ class odometry():
     pos = [0, 0, 0]
     rot = [0, 0, 0]
     running = True
+    markers = {}  # id : [corner1, corner2, corner3, corner4]
+    knownMarkers = {}
+
     def __init__(self, knownMarkers={}):
+        self.knownMarkers = knownMarkers
 
     def startOdometry(self):
         while(self.running=True):
-            pass
+            for marker in markers:
+                if marker in self.knownMarkers:
+                    pass
+                    # TODO get rvec, tvec, and use them to update self.pos and self.rot
+                else:
+                    pass
+                    # TODO add point to path for path planner
 
     def stopOdometry(self):
         self.running = False
